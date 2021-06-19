5fb780f9cc9a36b55f882d9b9e45217e467c943d
lith <serset@yeah.com>
2021-06-19 11:14:20 +0000

    auto commit 1.6

--- Publish/DevOps/github/70.docker-image-create.sh
+++ Publish/DevOps/github/70.docker-image-create.sh
@@ -26,12 +26,12 @@ dockerPath=$codePath/Publish/release/release/docker-image
 
 echo "copy dir"
 mkdir -p $dockerPath
-cp -rf $codePath/Publish/ReleaseFile/docker-image/. $dockerPath
+\cp -rf $codePath/Publish/ReleaseFile/docker-image/. $dockerPath
 
 
 #查找所有需要发布的项目并发布
 cd $codePath
-for file in \$(grep -a '<docker>' . -rl --include *.csproj)
+for file in $(grep -a '<docker>' . -rl --include *.csproj)
 do
 	cd $codePath
 	
@@ -44,7 +44,7 @@ do
 	echo create $dockerName
 
 	#copy file
-	cp -rf "$publishPath/$publishName" "$dockerPath/$dockerName/app"
+	\cp -rf "$publishPath/$publishName" "$dockerPath/$dockerName/app"
 done
 
 
